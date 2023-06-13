67931ef7867b9693800ab377f8cfff64f2f75f36
idealcapoeirasolanea <127629660+idealcapoeirasolanea@users.noreply.github.com>
2023-06-13 11:50:35 -0300

    Update

--- src/index.js
+++ src/index.js
@@ -3,8 +3,18 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import {HashRouter} from "react-router-dom";
 
+import {
+    createHashRouter,
+    RouterProvider
+} from 'react-router-dom';
+
+const router = createHashRouter([
+    {
+        path: "/*",
+        element: <App />,
+    }
+]);
+
 ReactDOM.render(<React.StrictMode>
-    <HashRouter>
-        <App/>
-    </HashRouter>
+        <RouterProvider router={router} />
 </React.StrictMode>, document.getElementById('root'),);
